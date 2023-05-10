28521b4ba83f9cebb4007b4d53db6f28754a05d0
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2023-05-10 14:35:50 +0200

    added crc_8 and tests folder

---
+++ tests/test_crc.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "../include/bitman.h"
+#include "tps_unit_test.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+void test_crc_8()
+{
+    uint8_t const message[] = "a";
+    uint8_t crc = crc_8(message, 1);
+    tps_assert(crc == 0x43);
+    uint16_t to_send = concat(1, message, crc);
+    tps_assert(to_send == 0b110000101000011); // contient le char 'a' et le crc
+    tps_assert(to_send >> 8 == 'a');
+    tps_assert(crc_8_check(to_send));
+}
+
+void unit_test_crc()
+{
+    //
+    test_crc_8();
+}
